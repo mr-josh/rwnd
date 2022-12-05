e300dd1c9a3d4d25ddc5015882db681bf87b7fbd
Mr Josh <mr-josh@sparklapse.com>
2022-12-06 10:07:21 +1100

    Added a single display ad to home

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -91,86 +91,113 @@ const App = () => {
             </Carousel.Slide>
           </Carousel>
         ) : (
-          <div className="start">
-            {!accessToken ? (
-              <Tooltip
-                label={
-                  <>
-                    <p>
-                      You don't have to do this if you're worried about
-                      security!
-                    </p>
-                    <p>You just won't see any channel profile pictures</p>
-                  </>
-                }
+          <>
+            <div className="start">
+              {!accessToken ? (
+                <Tooltip
+                  label={
+                    <>
+                      <p>
+                        You don't have to do this if you're worried about
+                        security!
+                      </p>
+                      <p>You just won't see any channel profile pictures</p>
+                    </>
+                  }
+                >
+                  <Button onClick={() => login()} color="teal" fullWidth>
+                    Login for Channel Profile Pictures
+                  </Button>
+                </Tooltip>
+              ) : (
+                <Notification
+                  icon={<IconCheck size={20} />}
+                  title="Success!"
+                  color="green"
+                  disallowClose
+                  radius="md"
+                >
+                  You're logged in and should see channel profile pictures.
+                </Notification>
+              )}
+              <Dropzone
+                getFilesFromEvent={(e) => {
+                  // Bug in @mantine/dropzone (https://github.com/mantinedev/mantine/issues/3115)
+                  return Promise.resolve([
+                    // @ts-ignore
+                    ...(e.target as EventTarget & HTMLInputElement)?.files,
+                  ]);
+                }}
+                onDrop={loadWatchHistory}
+                accept={["application/json"]}
               >
-                <Button onClick={() => login()} color="teal" fullWidth>
-                  Login for Channel Profile Pictures
+                <Group position="center" spacing="xl">
+                  <IconUpload size={40} />
+                  <div>
+                    <Text size="xl">Upload your watch-history.json</Text>
+                    <Text color="dimmed">
+                      Click here to select your watch-history.json file
+                    </Text>
+                    <Text color="dimmed">
+                      or drag and drop it here to upload
+                    </Text>
+                  </div>
+                </Group>
+              </Dropzone>
+              <Button.Group>
+                <Button
+                  component="a"
+                  href="/about"
+                  target="_blank"
+                  color="yellow"
+                >
+                  <span style={{ marginRight: "0.5rem" }}>About</span>
+                  <IconExternalLink size={16} />
                 </Button>
-              </Tooltip>
-            ) : (
-              <Notification
-                icon={<IconCheck size={20} />}
-                title="Success!"
-                color="green"
-                disallowClose
-                radius="md"
-              >
-                You're logged in and should see channel profile pictures.
-              </Notification>
-            )}
-            <Dropzone
-              getFilesFromEvent={(e) => {
-                // Bug in @mantine/dropzone (https://github.com/mantinedev/mantine/issues/3115)
-                return Promise.resolve([
-                  // @ts-ignore
-                  ...(e.target as EventTarget & HTMLInputElement)?.files,
-                ]);
-              }}
-              onDrop={loadWatchHistory}
-              accept={["application/json"]}
-            >
-              <Group position="center" spacing="xl">
-                <IconUpload size={40} />
-                <div>
-                  <Text size="xl">Upload your watch-history.json</Text>
-                  <Text color="dimmed">
-                    Click here to select your watch-history.json file
-                  </Text>
-                  <Text color="dimmed">or drag and drop it here to upload</Text>
-                </div>
-              </Group>
-            </Dropzone>
-            <Button.Group>
-              <Button
-                component="a"
-                href="/about"
-                target="_blank"
-                color="yellow"
-              >
-                <span style={{ marginRight: "0.5rem" }}>About</span>
-                <IconExternalLink size={16} />
-              </Button>
-              <Button
-                component="a"
-                href="/guide"
-                target="_blank"
-                color="yellow"
-              >
-                <span style={{ marginRight: "0.5rem" }}>Guide</span>
-                <IconExternalLink size={16} />
-              </Button>
-              <Button
-                component="a"
-                href="/privacy"
-                target="_blank"
-                color="yellow"
-              >
-                <span style={{ marginRight: "0.5rem" }}>Privacy</span>
-                <IconExternalLink size={16} />
-              </Button>
-            </Button.Group>
-          </div>
+                <Button
+                  component="a"
+                  href="/guide"
+                  target="_blank"
+                  color="yellow"
+                >
+                  <span style={{ marginRight: "0.5rem" }}>Guide</span>
+                  <IconExternalLink size={16} />
+                </Button>
+                <Button
+                  component="a"
+                  href="/privacy"
+                  target="_blank"
+                  color="yellow"
+                >
+                  <span style={{ marginRight: "0.5rem" }}>Privacy</span>
+                  <IconExternalLink size={16} />
+                </Button>
+              </Button.Group>
+              <hr style={{ width: "100%", borderColor: "white" }} />
+              <div className="ad">
+                <sup>
+                  This ad below helps pay to keep the website up. It's the only
+                  ad you'll ever see!
+                </sup>
+                <script
+                  async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4301858556408185"
+                  crossOrigin="anonymous"
+                ></script>
+                <ins
+                  className="adsbygoogle"
+                  style={{ display: "block" }}
+                  data-ad-client="ca-pub-4301858556408185"
+                  data-ad-slot="7625680943"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"
+                ></ins>
+                <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+              </div>
+            </div>
+          </>
         )}
       </div>
     </MantineProvider>
